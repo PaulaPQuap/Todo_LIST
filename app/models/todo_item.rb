7b75ece55d5f8c61d338af4bde60b9f572ec2a52
paulapquap <quap.paula@gmail.com>
2016-07-03 08:39:51 +0000

    almost finish

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,3 +1,8 @@
 class TodoItem < ActiveRecord::Base
   belongs_to :todo_list
+  
+  def completed?
+      !completed_at.blank?  # The complete_at colum should not be blank on date #
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   
   resources :todo_lists do  
-    resources :todo_items  # list all the items that belong that list
+    resources :todo_items do  # list all the items that belong that list
+      member do
+        patch :complete
+      end
+    end
   end
   
   root "todo_lists#index"
